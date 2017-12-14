3eb362d784aed727f1d9d15414fe93a6ff8c7621
saike1119 <saike1119@gmail.com>
2017-12-14 11:41:19 +0900

    ファイル修正

--- Assets/Scripts/Example.cs
+++ Assets/Scripts/Example.cs
@@ -5,9 +5,15 @@ using UnityEngine.SceneManagement;
 
 public class Example : MonoBehaviour {
 
+	public float rnd;
+
 	// Use this for initialization
 	void Start () {
+		rnd = Random.value;
+		Debug.Log (rnd);
 		Debug.Log (tasisan (33, 5));
+		Debug.Log (hikisan (100, 64));
+
 	}
 	
 	// Update is called once per frame
@@ -18,6 +24,13 @@ public class Example : MonoBehaviour {
 	public int tasisan(int x,int y){
 		int z;
 		z = x + y;
+		return z;
+	}
+
+	//引き算をするメソッド
+	public int hikisan(int x,int y){
+		int z;
+		z = x - y;
 		return z;
 	}
 }
