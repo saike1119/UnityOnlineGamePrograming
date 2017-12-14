399afe1af94e6e948841b8d846252b10c12151bb
saike1119 <saike1119@gmail.com>
2017-12-14 11:22:06 +0900

    example.cs追加

---
+++ Assets/Scripts/Example.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Example : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Debug.Log (tasisan (33, 5));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+
+	//足し算をするメソッド
+	public int tasisan(int x,int y){
+		int z;
+		z = x + y;
+		return z;
+	}
+}
